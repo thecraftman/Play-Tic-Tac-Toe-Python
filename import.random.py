951b03c99ae861cae25005405ae50249ed895d2c
Ore-Aruwaji, Oloruntola <temitopetola@gmail.com>
2019-02-12 07:35:09 +0100

    initial commit

---
+++ import.random.py
@@ -0,0 +1,13 @@
+import random
+
+def choose_first():
+    flip = random.randint(0,1)
+
+    if flip == 0:
+        return 'player 1'
+    else:
+        return 'player 2'
+
+
+def space_check(board, position):
+    return board[position] == ' '
