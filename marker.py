1483e3266f0561426d33cfbceb700f14909438de
Ore-Aruwaji, Oloruntola <temitopetola@gmail.com>
2019-02-12 07:19:23 +0100

    initial commit

---
+++ marker.py
@@ -0,0 +1,10 @@
+def player_input():
+    marker = ''
+
+    while marker != 'X' and marker !='O':
+        marker = input ('player1: choose X or O : ').upper()
+
+        if marker == 'X':
+            return ('X','O')
+        else:
+            return ('O','X')
